fbd385bb1c56b8a75a15413728b2454a721c8767
samulacraclawmachine <samulacraclawmachine@gmail.com>
2024-08-10 18:51:42 -0400

    poopoop

--- TwitchPlays_TEMPLATE.py
+++ TwitchPlays_TEMPLATE.py
@@ -40,14 +40,14 @@ pyautogui.FAILSAFE = False
 ESP32_IP = "192.168.0.42"  # Replace with your ESP32's IP address
 PORT = 80
 
-def send_data_to_esp32(data):
+def send_command_to_esp32(command):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((ESP32_IP, PORT))
-            s.sendall(data.encode('utf-8'))
-            print(f"Sent data to ESP32: {data}")
+            s.sendall(command.encode('utf-8'))
+            print(f"Sent command to ESP32: {command}")
     except Exception as e:
-        print(f"Failed to send data to ESP32: {e}")
+        print(f"Failed to send command to ESP32: {e}")
 
 ##########################################################
 
@@ -72,68 +72,24 @@ def handle_message(message):
 
         print("Got this message from " + username + ": " + msg)
 
-        # Send the message to the ESP32
-        send_data_to_esp32(msg)
-
-        # Now that you have a chat message, this is where you add your game logic.
-        # Use the "HoldKey(KEYCODE)" function to permanently press and hold down a key.
-        # Use the "ReleaseKey(KEYCODE)" function to release a specific keyboard key.
-        # Use the "HoldAndReleaseKey(KEYCODE, SECONDS)" function press down a key for X seconds, then release it.
-        # Use the pydirectinput library to press or move the mouse
-
-        # I've added some example videogame logic code below:
-
-        ###################################
-        # Example GTA V Code 
-        ###################################
-
-        # If the chat message is "left", then hold down the A key for 2 seconds
+        # Send the relevant command to the ESP32
         if msg == "left": 
-            HoldAndReleaseKey(A, 2)
-
-        # If the chat message is "right", then hold down the D key for 2 seconds
-        if msg == "right": 
-            HoldAndReleaseKey(D, 2)
-
-        # If message is "drive", then permanently hold down the W key
-        if msg == "drive": 
-            ReleaseKey(S)  # release brake key first
-            HoldKey(W)  # start permanently driving
-
-        # If message is "reverse", then permanently hold down the S key
-        if msg == "reverse": 
-            ReleaseKey(W)  # release drive key first
-            HoldKey(S)  # start permanently reversing
-
-        # Release both the "drive" and "reverse" keys
-        if msg == "stop": 
-            ReleaseKey(W)
-            ReleaseKey(S)
-
-        # Press the spacebar for 0.7 seconds
-        if msg == "brake": 
-            HoldAndReleaseKey(SPACE, 0.7)
-
-        # Press the left mouse button down for 1 second, then release it
-        if msg == "shoot": 
-            pydirectinput.mouseDown(button="left")
-            time.sleep(1)
-            pydirectinput.mouseUp(button="left")
-
-        # Move the mouse up by 30 pixels
-        if msg == "aim up":
-            pydirectinput.moveRel(0, -30, relative=True)
-
-        # Move the mouse right by 200 pixels
-        if msg == "aim right":
-            pydirectinput.moveRel(200, 0, relative=True)
-
-        ####################################
-        ####################################
+            send_command_to_esp32('L')
+        elif msg == "right": 
+            send_command_to_esp32('R')
+        elif msg == "drive": 
+            send_command_to_esp32('F')
+        elif msg == "reverse": 
+            send_command_to_esp32('B')
+        elif msg == "stop": 
+            send_command_to_esp32('S')
+        elif msg == "up":
+            send_command_to_esp32('U')
+        elif msg == "down":
+            send_command_to_esp32('D')
 
     except Exception as e:
         print("Encountered exception: " + str(e))
-
 
 while True:
 
