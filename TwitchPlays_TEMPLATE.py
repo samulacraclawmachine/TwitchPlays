2ad2ecfa33c433913033e299692dbe13fd73b8e8
samulacraclawmachine <samulacraclawmachine@gmail.com>
2024-09-07 12:18:27 -0400

    chase97

--- TwitchPlays_TEMPLATE.py
+++ TwitchPlays_TEMPLATE.py
@@ -131,8 +131,8 @@ if __name__ == "__main__":
                 send_command_to_esp32('B')
             elif msg == "stop": 
                 send_command_to_esp32('S')
-            # elif msg == "up":
-            #     send_command_to_esp32('U')
+            elif msg == "chase97":
+                send_command_to_esp32('U')
             # elif msg == "down":
             #     send_command_to_esp32('D')
             # elif msg == "open":
